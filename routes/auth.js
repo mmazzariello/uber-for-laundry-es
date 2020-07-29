ecf0a6e6ce3d5f448222741d8b5e18609b1040b0
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-29 12:00:07 +0200

    logout is done

--- routes/auth.js
+++ routes/auth.js
@@ -113,11 +113,32 @@ authRouter.post("/login", (req, res, next) => {
         res.render("auth/login", {
           errorMessage: "Incorrect password",
         });
+        return;
       }
+      //seria que este aca
     })
     .catch((error) => {
       next(error);
     });
 });
+
+//LOGOUT
+
+authRouter.get('/logout', (req,res,next) => {
+  if(!req.session.currentUser) {
+    res.redirect('/');
+    return;
+  }
+
+  req.session.destroy((err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    res.redirect('/');
+  })
+})
+
 
 module.exports = authRouter;
